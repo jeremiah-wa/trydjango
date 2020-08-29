6a1ea2365aca6a2cae796e8853fd06c39ffbcddc
Jeremiah Wangaruro <jeremiah.wangaruro@ucdconnect.ie>
2020-08-29 22:05:54 +0100

    added product and page views

--- products/views.py
+++ products/views.py
@@ -1,9 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.forms import forms
+from .models import Product
+from .form import ProductForm
+
+# Create your views here
 
 
-# Create your views here.
-def home_view(request, *args, **kargs):
-    # return HttpResponse("<h1>Hello World</h1>")
-    print(request.user)
-    return render(request, "home.html", {})
+# TODO
+"""
+    product_create_view,
+    product_detail_view,
+    product_list_view
+"""
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'products/create.html', context)
+
+
+def product_detail_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        'object': obj
+    }
+    return render(request, "products/detail.html", context)
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/product_list.html", context)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from src.products.views import home_view
+from django.urls import path, include
+from src.pages.views import home_view, about_view, contact_view
 
 urlpatterns = [
-    path('', home_view),
+    path('', home_view, name='home'),
+    path('about/', about_view),
+    path('contact/', contact_view),
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
 ]
